54314c620821807418f8c220246cf9b413c619f3
Shubham Shah <lifewithspike@hotmail.com>
2020-07-17 14:22:32 -0400

    First commit

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, render_template, redirect, session, make_response, url_for, flash, jsonify
 import requests
-import dropbox
 import json
-from datetime import datetime, timedelta
-import time
-from flask_simple_geoip import SimpleGeoIP
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
+import config
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "thisisasecret"
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['GOOGLEMAPS_KEY'] = config.GOOGLEMAPS_KEY
 
+
+GoogleMaps(app)
 
 @app.route("/", methods = ["get"])
 def index():
-    locationJSON = requests.get("http://ip-api.com/json/" + request.headers['X-Forwarded-For']).json()
+    locationJSON = requests.get("http://ip-api.com/json/" + "99.228.3.238").json()
     lat = str(locationJSON['lat'])
     lng = str(locationJSON['lon'])
 
@@ -28,16 +31,42 @@ def index():
     businessJSON = []
     businessJSON.extend(firstJSON['businesses'])
     businessJSON.extend(secondJSON['businesses'])
-    print(businessJSON)
-    return render_template('index.html', businesses=businessJSON)
+
+    markerList = []
+
+    for i in businessJSON:
+        payload = {
+            "icon": "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+            "lat":i["coordinates"]["latitude"],
+            "lng":i["coordinates"]["longitude"],
+            'infobox': "<img style='width:150px;height:150px' src=" + i["image_url"]+" />"+"\n"+i['name'],
+        }
+
+        markerList.append(payload)
+
+    sndmap = Map(
+        identifier="sndmap",
+        lat=lat,
+        lng=lng,
+        style= "height:45vh;width:70vw;margin-bottom:7vh;color:black;",
+        markers=markerList
+    )
+
+    return render_template('index.html', businesses=businessJSON, sndmap=sndmap)
 
 @app.route("/test")
 def test():
     return jsonify({'ip': request.headers['X-Forwarded-For']}), 200
 
-@app.route('/gofundme')
-def gofundme():
-    return render_template('gofundme.html')
+@app.route('/business/<id>')
+def business(id):
+    headers = {
+        "Authorization": "Bearer OuzpIYU03EajimgwSE7bOGjgdqRsjyI8zGunpx6DR1d-LgNjk8K-ioSLjf2_g57n5xcMD4meWXFsUf5rGJo63q5yqFUMWxIYoVwRJTDFxRGQNhd3zjWI72Sh0xsRX3Yx"
+      }
+
+    businessJSON = requests.get("https://api.yelp.com/v3/businesses/" + id, headers=headers).json()
+
+    return render_template('business.html', business=businessJSON)
 
 if(__name__ == "__main__"):
     app.run(debug=True)
